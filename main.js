4114e4067752409eed8d21b98fbb40924087f4c6
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2022-03-07 02:24:34 +0000

    updating frontpage

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-// document.getElementById('rimOrdabok').addEventListener('click', function(event) {
-
-// })
+// var b = document.getElementById('body')
+// for (var i = 0; i < 100; i++) {
+//     var n = 'github' + i
+//     b.innerHTML += '    <a href="https://github.com/danielfreyrg/danielfreyrg.github.io"><div class="link" id="github">' + n + '</div></a>'
+// }
