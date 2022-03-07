9f0701b46be474e1bfba5bd9b14162413dca716b
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2022-03-07 03:33:05 +0000

    good night

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// var b = document.getElementById('body')
+// var b = document.getElementById('links')
 // for (var i = 0; i < 100; i++) {
 //     var n = 'github' + i
 //     b.innerHTML += '    <a href="https://github.com/danielfreyrg/danielfreyrg.github.io"><div class="link" id="github">' + n + '</div></a>'
