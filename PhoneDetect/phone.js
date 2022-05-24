bbe82df0ed6abd24a77268c1bcf03eebbbaeb881
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2022-05-24 21:52:51 +0000

    Update phone.js

--- PhoneDetect/phone.js
+++ PhoneDetect/phone.js
@@ -3,10 +3,10 @@ const check = function() {
     document.getElementById("phone").innerHTML = ua
     if (navigator.userAgent.match(/Android/i)) {
         console.log('Android');
-        document.location.href = "apple.com"
+        document.location.href = "https://www.apple.com"
     } else if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
         console.log('iOS');
-        document.location.href = "android.com"
+        document.location.href = "https://www.android.com"
     }
     console.log(ua);
 }
