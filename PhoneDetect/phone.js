d703aab2c5cbaed90309efaf80f20cce26d86044
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2022-05-24 19:21:15 +0000

    bug

--- PhoneDetect/phone.js
+++ PhoneDetect/phone.js
@@ -1,9 +1,5 @@
-document.onload = function() {
-    //detect if the user has a iphone or android phone
+const check = function() {
     var ua = navigator.userAgent.toLowerCase();
-    if (ua.indexOf("iphone") > -1 || ua.indexOf("android") > -1) {
-        //if the user has a phone, hide the phone number and show the phone icon
-        document.getElementById("phone").innerHTML = ua
-    }
-
+    document.getElementById("phone").innerHTML = ua
+    console.log(ua);
 }
