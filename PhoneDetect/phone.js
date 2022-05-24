6d2bc47f24116d3710689a6e6e9cc46b06563d94
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2022-05-24 21:57:01 +0000

    Update phone.js

--- PhoneDetect/phone.js
+++ PhoneDetect/phone.js
@@ -3,12 +3,9 @@ const check = function() {
     document.getElementById("phone").innerHTML = ua
     if (navigator.userAgent.match(/Android/i)) {
         console.log('Android');
-        document.location.href = "https://www.apple.com"
+        document.location.href = "https://www.android.com"
     } else if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
         console.log('iOS');
-        document.location.href = "https://www.android.com"
+        document.location.href = "https://www.apple.com"
     }
-    console.log(ua);
 }
-
-//check if the user is using a android or an ios phone
