17552300b318f312980875d1d709ecf1988e43ce
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2023-02-07 19:32:38 +0000

    skala

--- Svgtest/test.js
+++ Svgtest/test.js
@@ -4,119 +4,116 @@ var svg = document.querySelector('svg');
 svg.viewBox.viewBox = '0 0 ' + screenwidth + ' ' + screenheight;
 console.log(svg.viewBox);
 var img = document.querySelector('img');
-// window.addEventListener('resize', function(){
-// 	svg.style.maxHeight = img.clientHeight + 'px';
-// 	console.log(svg.style.maxHeight);
-// })
+ 
 
-// document.getElementById('middle').addEventListener('click', function() {
-// 	// document.getElementById('building2-checkbox').checked = !document.getElementById('building2-checkbox').checked;
-// 	document.getElementById('building2-checkbox').click();
-// 	document.getElementById('left-div').classList.remove('open-left');
-// 	document.getElementById('middle-div').classList.toggle('open');
-// })
+document.getElementById('middle').addEventListener('click', function() {
+	// document.getElementById('building2-checkbox').checked = !document.getElementById('building2-checkbox').checked;
+	document.getElementById('building2-checkbox').click();
+	document.getElementById('left-div').classList.remove('open-left');
+	document.getElementById('middle-div').classList.toggle('open');
+})
 
-// document.getElementById('left').addEventListener('click', function() {
-// 	// document.getElementById('building1-checkbox').checked = !document.getElementById('building1-checkbox').checked;
-// 	document.getElementById('building1-checkbox').click();
-// 	document.getElementById('middle-div').classList.remove('open');
-// 	document.getElementById('left-div').classList.toggle('open-left');
-// })
-// document.getElementById('right').addEventListener('click', function() {
-// 	document.getElementById('middle-div').classList.remove('open');
-// 	document.getElementById('left-div').classList.remove('open-left');
-// 	// document.getElementById('building3-checkbox').checked = !document.getElementById('building3-checkbox').checked;
-// document.getElementById('building3-checkbox').click();
+document.getElementById('left').addEventListener('click', function() {
+	// document.getElementById('building1-checkbox').checked = !document.getElementById('building1-checkbox').checked;
+	document.getElementById('building1-checkbox').click();
+	document.getElementById('middle-div').classList.remove('open');
+	document.getElementById('left-div').classList.toggle('open-left');
+})
+document.getElementById('right').addEventListener('click', function() {
+	document.getElementById('middle-div').classList.remove('open');
+	document.getElementById('left-div').classList.remove('open-left');
+	// document.getElementById('building3-checkbox').checked = !document.getElementById('building3-checkbox').checked;
+document.getElementById('building3-checkbox').click();
 
-// })
-// document.getElementById('building1').addEventListener('click', function() {
-// 	document.getElementById('middle-div').classList.remove('open');
-// 	document.getElementById('left-div').classList.toggle('open-left');
-// })
-// document.getElementById('building2').addEventListener('click', function() {
-// 	document.getElementById('middle-div').classList.toggle('open');
-// 	document.getElementById('left-div').classList.remove('open-left');
-// })
-// document.getElementById('building3').addEventListener('click', function() {
-// 	document.getElementById('middle-div').classList.remove('open');
-// 	document.getElementById('left-div').classList.remove('open-left');
-// })
-// document.getElementById('left').addEventListener('mouseover', function() {
+})
+document.getElementById('building1').addEventListener('click', function() {
+	document.getElementById('middle-div').classList.remove('open');
+	document.getElementById('left-div').classList.toggle('open-left');
+})
+document.getElementById('building2').addEventListener('click', function() {
+	document.getElementById('middle-div').classList.toggle('open');
+	document.getElementById('left-div').classList.remove('open-left');
+})
+document.getElementById('building3').addEventListener('click', function() {
+	document.getElementById('middle-div').classList.remove('open');
+	document.getElementById('left-div').classList.remove('open-left');
+})
+document.getElementById('left').addEventListener('mouseover', function() {
 
-// 	document.getElementById('building1').classList.add('hover')
-// })
-// document.getElementById('left').addEventListener('mouseout', function() {
+	document.getElementById('building1').classList.add('hover')
+})
+document.getElementById('left').addEventListener('mouseout', function() {
 
-// 	document.getElementById('building1').classList.remove('hover')
-// })
-// document.getElementById('middle').addEventListener('mouseover', function() {
+	document.getElementById('building1').classList.remove('hover')
+})
+document.getElementById('middle').addEventListener('mouseover', function() {
 
-// 	document.getElementById('building2').classList.add('hover')
+	document.getElementById('building2').classList.add('hover')
 
 
-// })
-// document.getElementById('middle').addEventListener('mouseout', function() {
+})
+document.getElementById('middle').addEventListener('mouseout', function() {
 
-// 	document.getElementById('building2').classList.remove('hover')
+	document.getElementById('building2').classList.remove('hover')
 
-// })
+})
 
-// document.getElementById('right').addEventListener('mouseover', function() {
+document.getElementById('right').addEventListener('mouseover', function() {
 
-// 	document.getElementById('building3').classList.add('hover')
-// })
-// document.getElementById('right').addEventListener('mouseout', function() {
+	document.getElementById('building3').classList.add('hover')
+})
+document.getElementById('right').addEventListener('mouseout', function() {
 
-// 	document.getElementById('building3').classList.remove('hover')
+	document.getElementById('building3').classList.remove('hover')
 	
 
-// })
+})
 
-// document.getElementById('building1').addEventListener('mouseover', function() {
+document.getElementById('building1').addEventListener('mouseover', function() {
 
-// 	document.getElementById('left').classList.add('buildinghover')
-// })
-// document.getElementById('building1').addEventListener('mouseout', function() {
+	document.getElementById('left').classList.add('buildinghover')
+})
+document.getElementById('building1').addEventListener('mouseout', function() {
 
-// 	document.getElementById('left').classList.remove('buildinghover')
-// }
-// )
-// document.getElementById('building2').addEventListener('mouseover', function() {
+	document.getElementById('left').classList.remove('buildinghover')
+}
+)
+document.getElementById('building2').addEventListener('mouseover', function() {
 
-// 	document.getElementById('middle').classList.add('buildinghover')
-// }
-// )	
-// document.getElementById('building2').addEventListener('mouseout', function() {
+	document.getElementById('middle').classList.add('buildinghover')
+}
+)	
+document.getElementById('building2').addEventListener('mouseout', function() {
 
-// 	document.getElementById('middle').classList.remove('buildinghover')
-// }
-// )
-// document.getElementById('building3').addEventListener('mouseover', function() {
+	document.getElementById('middle').classList.remove('buildinghover')
+}
+)
+document.getElementById('building3').addEventListener('mouseover', function() {
 
-// 	document.getElementById('right').classList.add('buildinghover')
-// }
-// )
-// document.getElementById('building3').addEventListener('mouseout', function() {
+	document.getElementById('right').classList.add('buildinghover')
+}
+)
+document.getElementById('building3').addEventListener('mouseout', function() {
 
-// 	document.getElementById('right').classList.remove('buildinghover')
-// }
-// )
+	document.getElementById('right').classList.remove('buildinghover')
+}
+)
 
-// document.getElementById('building1-checkbox').addEventListener('change', function() {
-// 	document.getElementById('building1').classList.toggle('checked')
-// 	document.getElementById('building2').classList.remove('checked')
-// 	document.getElementById('building3').classList.remove('checked')
-// }
-// )
-// document.getElementById('building2-checkbox').addEventListener('change', function() {
-// 	document.getElementById('building2').classList.toggle('checked')
-// 	document.getElementById('building1').classList.remove('checked')
-// 	document.getElementById('building3').classList.remove('checked')
-// }
-// )
-// document.getElementById('building3-checkbox').addEventListener('change', function() {
-// 	document.getElementById('building3').classList.toggle('checked')
-// 	document.getElementById('building1').classList.remove('checked')
-// 	document.getElementById('building2').classList.remove('checked')
-// }
-// )
+document.getElementById('building1-checkbox').addEventListener('change', function() {
+	document.getElementById('building1').classList.toggle('checked')
+	document.getElementById('building2').classList.remove('checked')
+	document.getElementById('building3').classList.remove('checked')
+}
+)
+document.getElementById('building2-checkbox').addEventListener('change', function() {
+	document.getElementById('building2').classList.toggle('checked')
+	document.getElementById('building1').classList.remove('checked')
+	document.getElementById('building3').classList.remove('checked')
+}
+)
+document.getElementById('building3-checkbox').addEventListener('change', function() {
+	document.getElementById('building3').classList.toggle('checked')
+	document.getElementById('building1').classList.remove('checked')
+	document.getElementById('building2').classList.remove('checked')
+}
+)
