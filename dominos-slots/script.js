fce00e4aaf029b6a2ba1613d65faa60dfe617fda
Daníel Freyr Gylfason <danielfg19@ru.is>
2024-01-09 17:54:13 +0000

    slots

--- dominos-slots/script.js
+++ dominos-slots/script.js
@@ -24,7 +24,7 @@ function spin() {
     var random = Math.floor(Math.random() * 6);
     //move the wheel by 33% * a random number (0-5) to choose the right position for the slot then add 2000 to simulate a full casino spin (10*200 so it always spins atleast 10 times) 
     //100% background position y moves the slots up 3 places, 200% is a whole spin
-    positionY = random * (100 / 3) + -1750 + "%";
+    positionY = random * (100 / 3) + -2000 + "%";
     // roll.push(prizes[random]);
     if (index === 0) {
       roll.push(slots_left[random]);
