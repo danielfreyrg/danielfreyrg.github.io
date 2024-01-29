a6d9f782f8f2cdec5e23f03c1550b8e2e452164f
Daníel Freyr Gylfason <danielfg19@ru.is>
2024-01-29 15:04:23 +0000

    sjuk ást

---
+++ sjukast/script.js
@@ -0,0 +1,45 @@
+let counter = 0;
+var realRotation = 0;
+
+// Function to update the counter
+function updateCounter(isChecked, isFormKk) {
+    // Increment or decrement the counter by 2 based on the checkbox state and form
+    if (isChecked && isFormKk) {
+        counter += 2;
+    } else if (isChecked && !isFormKk) {
+        counter -= 2;
+    } else if (!isChecked && isFormKk) {
+        counter -= 2;
+    } else if (!isChecked && !isFormKk && counter) {
+        counter += 2;
+    }
+    if (counter > 10) {
+        realRotation = 10;
+    }
+    else {
+        realRotation = counter;
+    }
+    if (counter < -10) {
+        realRotation = -10;
+    }
+    else {
+        realRotation = counter;
+    }
+    document.querySelector('.top').style.transform = `rotate(${realRotation}deg)`; 
+    // Log the current counter value for demonstration
+    console.log("Counter value:", counter);
+}
+
+// Add event listeners to checkboxes in the 'kk' form
+document.querySelectorAll('#kk input[type="checkbox"]').forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+        updateCounter(this.checked, true);
+    });
+});
+
+// Add event listeners to checkboxes in the 'kvk' form
+document.querySelectorAll('#kvk input[type="checkbox"]').forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+        updateCounter(this.checked, false);
+    });
+});
