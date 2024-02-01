282e81c14943abb63ae9f903c00e1392160ca0e2
Daníel Freyr Gylfason <danielfg19@ru.is>
2024-02-01 10:22:18 +0000

    main

--- sjukast/script.js
+++ sjukast/script.js
@@ -1,141 +1,286 @@
 let counter = 0;
 var realRotation = 0;
 var placedwords = 0;
+var dropzone
+var startzone
+var placedwords = 0
+var animationIn = 'slide-in'
+var animationOut = 'slide-out'
+if (window.innerWidth < 600) {
+    animationIn = 'slide-in-mobile'
+    animationOut = 'slide-out-mobile'
+}
 // JavaScript objects to hold the weights for checkboxes
-const maleWordWeights = {
-    "kk-yngri": 1,
-    "kk-ekki-med-fulla-medvitund": 1,
-    "kk-feiminn": 1,
-    "kk-ad-vinna-ur-afalli": 1,
-    "kk-ooruggur": 1,
-    "kk-i-ojafnvaegi": 1,
-    "kk-hraeddur": 1,
-    "kk-frosinn": 1,
-    "kk-ekki-i-godu-astandi": 1,
-    "kk-oreyndur": 1,
-    "kk-likamlega-sterkur": 2,
-    "kk-fraegur": 2,
-    "kk-frekur": 2,
-    "kk-vinsaell": 2,
-    "kk-sjalfsoruggur": 2,
-    "kk-aestur": 2,
-    "kk-eldri": 2,
-    "kk-med-meiri-reynslu": 2,
-    "kk-efnadur": 2,
-    "kk-godur-i-kjaftinum": 2
+const wordWeights = {
+    "yngri": -1,
+    "ekki-med-fulla-medvitund": -1,
+    "feiminn": -1,
+    "ad-vinna-ur-afalli": -1,
+    "ooruggur": -1,
+    "i-ojafnvaegi": -1,
+    "hraeddur": -1,
+    "frosinn": -1,
+    "ekki-i-godu-astandi": -1,
+    "oreyndur": -1,
+    "likamlega-sterkur": 2,
+    "fraegur": 2,
+    "frekur": 2,
+    "vinsaell": 2,
+    "sjalfsoruggur": 2,
+    "aestur": 2,
+    "eldri": 2,
+    "med-meiri-reynslu": 2,
+    "efnadur": 2,
+    "godur-i-kjaftinum": 2
 };
-
-const femaleWordWeights = {
-    "kvk-yngri": -1,
-    "kvk-ekki-med-fulla-medvitund": -1,
-    "kvk-feimin": -1,
-    "kvk-ad-vinna-ur-afalli": -1,
-    "kvk-oorugg": -1,
-    "kvk-i-ojafnvaegi": -1,
-    "kvk-hraedd": -1,
-    "kvk-frosin": -1,
-    "kvk-ekki-i-godu-astandi": -1,
-    "kvk-oreynd": -1,
-    "kvk-likamlega-sterk": -2,
-    "kvk-fraeg": -2,
-    "kvk-frek": -2,
-    "kvk-vinsael": -2,
-    "kvk-sjalfsorugg": -2,
-    "kvk-aest": -2,
-    "kvk-eldri": -2,
-    "kvk-med-meiri-reynslu": -2,
-    "kvk-efnud": -2,
-    "kvk-god-i-kjaftinum": -2
+const wordsKK = {
+    "yngri": "yngri",
+    "ekki-med-fulla-medvitund": "ekki með fulla meðvitund",
+    "feiminn": "feiminn",
+    "ad-vinna-ur-afalli": "að vinna úr áfalli",
+    "ooruggur": "óöruggur",
+    "i-ojafnvaegi": "í ójafnvægi",
+    "hraeddur": "hræddur",
+    "frosinn": "frosinn",
+    "ekki-i-godu-astandi": "ekki í góðu ástandi",
+    "oreyndur": "óreyndur",
+    "likamlega-sterkur": "líkamlega sterkur",
+    "fraegur": "frægur",
+    "frekur": "frekur",
+    "vinsaell": "vinsæll",
+    "sjalfsoruggur": "sjálfsöruggur",
+    "aestur": "æstur",
+    "eldri": "eldri",
+    "med-meiri-reynslu": "með meiri reynslu",
+    "efnadur": "efnadur",
+    "godur-i-kjaftinum": "góður í kjaftinum"
 };
-function updateDom(checkbox, isFormKk) {
- var label = document.querySelector('label[for="' + checkbox.id + '"]').textContent;
- var exists = document.getElementById("word-" + checkbox.id);
-     if (isFormKk) {
-        if (exists) {
-            exists.style.animationName = 'fade-out-right';
-            exists.addEventListener('animationend', function() {
-            exists.remove();
-            placedwords -= 1;
-            });
-        }
-        else {
-        placedwords += 1;
-    document.querySelector('.kk-words').innerHTML += `<div class="word" id="word-${checkbox.id}" style="animation-name: slide-down;">${label}</div>`;
-    document.querySelectorAll('.word').forEach(function(word) {
-        word.addEventListener('animationend', function() {
-            word.style.animationName = 'none';
+const wordsHK = {
+    "yngri": "yngri",
+    "ekki-med-fulla-medvitund": "ekki með fulla meðvitund",
+    "feiminn": "feimið",
+    "ad-vinna-ur-afalli": "að vinna úr áfalli",
+    "ooruggur": "óöruggt",
+    "i-ojafnvaegi": "í ójafnvægi",
+    "hraeddur": "hrætt",
+    "frosinn": "frosið",
+    "ekki-i-godu-astandi": "ekki í góðu ástandi",
+    "oreyndur": "óreynt",
+    "likamlega-sterkur": "líkamlega sterkt",
+    "fraegur": "frægt",
+    "frekur": "frekt",
+    "vinsaell": "vinsælt",
+    "sjalfsoruggur": "sjálfsöruggt",
+    "aestur": "æst",
+    "eldri": "eldri",
+    "med-meiri-reynslu": "með meiri reynslu",
+    "efnadur": "efnað",
+    "godur-i-kjaftinum": "gott í kjaftinum"
+};
+const wordsKVK = {
+    "yngri": "yngri",
+    "ekki-med-fulla-medvitund": "ekki með fulla meðvitund",
+    "feiminn": "feimin",
+    "ad-vinna-ur-afalli": "að vinna úr áfalli",
+    "ooruggur": "óörugg",
+    "i-ojafnvaegi": "í ójafnvægi",
+    "hraeddur": "hrædd",
+    "frosinn": "frosin",
+    "ekki-i-godu-astandi": "ekki í góðu ástandi",
+    "oreyndur": "óreynd",
+    "likamlega-sterkur": "líkamlega sterk",
+    "fraegur": "fræg",
+    "frekur": "frek",
+    "vinsaell": "vinsæl",
+    "sjalfsoruggur": "sjálfsörugg",
+    "aestur": "æst",
+    "eldri": "eldri",
+    "med-meiri-reynslu": "með meiri reynslu",
+    "efnadur": "efnuð",
+    "godur-i-kjaftinum": "góð í kjaftinum"
+};
+document.addEventListener('DOMContentLoaded', function () {
+    // Make words draggable
+    const words = document.querySelectorAll('label');
+    words.forEach(word => {
+        word.setAttribute('draggable', true);
+        word.addEventListener('dragstart', (event) => {
+            startzone = event.target.parentElement
+            event.dataTransfer.setData('text/plain', event.target.id);
         });
-    }
-    )
+    });
+
+    // Setup drop zones
+    const dropZones = document.querySelectorAll('.right-words, .left-words, .start');
+    dropZones.forEach(zone => {
+        zone.addEventListener('dragover', (event) => {
+            event.preventDefault(); // Allow dropping by preventing default
+        });
+
+        zone.addEventListener('drop', (event) => {
+            event.preventDefault();
+            document.querySelector('.info').style.animationName = 'fade-out';
+            const id = event.dataTransfer.getData('text/plain');
+            const draggableElement = document.getElementById(id);
+            zone.appendChild(draggableElement);
+            draggableElement.style.animationName = 'slide-down';
+            draggableElement.addEventListener('animationend', function () {
+                draggableElement.style.animationName = 'none';
+            });
+
+            updateCounter(zone, id); // Update counters or any other logic after drop
+        });
+    });
+
+    // Function to update counters based on current dropped words
+    function updateCounter(dropzone, id) {
+        var alreadyplaced = false
+        var weight = wordWeights[id];
+        weight = weight * 2;
+
+        if (dropzone.classList.contains('left-words')) {
+            weight = -weight;
         }
+        if (startzone.classList.contains('left-words') && dropzone.classList.contains('right-words') 
+        || startzone.classList.contains('right-words') && dropzone.classList.contains('left-words')) {
+            alreadyplaced = true
+            weight = weight * 2;
+        }
+        if (dropzone.classList.contains('start') && !startzone.classList.contains('left-words')) {
+
+            weight = -weight;
+
+        }
+        if (dropzone.classList.contains('start')) {
+            alreadyplaced = true
+            weight = weight * 2;
+        }
+
+        if (startzone == dropzone) {
+            weight = 0;
+            alreadyplaced = true
+        }
+
+        if (!alreadyplaced) {
+            placedwords += 1;
+        }
+        if (placedwords > 3) {
+            //wait for 3 seconds
+            setTimeout(function () {
+            document.querySelector('.scene3').style.animationName = 'slide-out';
+            document.querySelector('.scene3').addEventListener('animationend', function () {
+                document.querySelector('.scene4').style.display = 'block';
+                document.querySelector('.background-plain').style.display = 'block'
+                document.querySelector('.scene3').style.display = 'none'
+
+
+            }
+            )
+            
+            }, 1500)
+        }
+
+        counter += weight;
+        rotateBar()
+
+        console.log('------------------')
+
+
+
+    }
+});
+
+
+
+function rotateBar() {
+
+    if (counter > 11) {
+        realRotation = 11;
+    } else if (counter < -11) {
+        realRotation = -11;
     }
     else {
-        if (exists) {
-            exists.style.animationName = 'fade-out-left';
-            exists.addEventListener('animationend', function() {
-            exists.remove();
-            });
-            placedwords -= 1;
-        }
-        else {
-        document.querySelector('.kvk-words').innerHTML += `<div class="word" id="word-${checkbox.id}" style="animation-name: slide-down;">${label}</div>`;
-        placedwords
-        document.querySelectorAll('.word').forEach(function(word) {
-            word.addEventListener('animationend', function() {
-                word.style.animationName = 'none';
-            });
-        }
-        )
-    }
-}
-// if (placedwords > 8) {
-//     document.querySelector('.words').style.justifyContent = 'space-evenly'
-
-// } else {
-//     document.querySelector('.words').style.justifyContent = ''
-// }
-}
-
-// Function to update the counter
-function updateCounter(checkbox, isFormKk) {
-    const isChecked = checkbox.checked;
-    const checkboxId = checkbox.id;
-    updateDom(checkbox, isFormKk);
-
-    // Use checkboxId to access the weight
-    const weight = isFormKk ? maleWordWeights[checkboxId] : femaleWordWeights[checkboxId];
-    console.log("Weight:", weight);
-
-    if (isChecked) {
-        counter += weight;
-    } else {
-        counter -= weight;
-    }
-
-    if (counter > 10) {
-        realRotation = 10;
-    } else if (counter < -10) {
-        realRotation = -10;
-    } else {
         realRotation = counter;
     }
-
-    document.querySelector('.top').style.transform = `rotate(${realRotation}deg)`;
-    document.querySelector('.words').style.transform = `rotate(${realRotation}deg)`;
-    // Log the current counter value for demonstration
-    console.log("Counter value:", counter);
+    document.querySelector('.scene3 .bar').style.transform = `rotate(${realRotation}deg)`;
+    document.querySelector('.scene3 .words').style.transform = `rotate(${realRotation}deg)`;
 }
 
-// Add event listeners to checkboxes in the 'kk' form
-document.querySelectorAll('#kk input[type="checkbox"]').forEach(function(checkbox) {
-    checkbox.addEventListener('change', function() {
-        updateCounter(this, true); // Pass `this` (the checkbox) directly
-    });
-});
 
-// Add event listeners to checkboxes in the 'kvk' form
-document.querySelectorAll('#kvk input[type="checkbox"]').forEach(function(checkbox) {
-    checkbox.addEventListener('change', function() {
-        updateCounter(this, false); // Pass `this` (the checkbox) directly
-    });
-});
+
+
+
+
+document.querySelector('.kk-button').addEventListener('click', function () {
+    document.querySelectorAll('form > label').forEach(function (label) {
+        label.innerHTML = wordsKK[label.id];
+    })
+    document.querySelector('.scene2').style.animationName = animationOut;
+    document.querySelector('.scene2').addEventListener('animationend', function () {
+        document.querySelector('.scene3').style.animationDuration = '0.7s';
+        document.querySelector('.scene3').style.display = 'block';
+        document.querySelector('.scene3').style.animationName = animationIn;
+        document.querySelector('.scene2').style.display = 'none'
+
+    })
+
+
+
+})
+document.querySelector('.kvk-button').addEventListener('click', function () {
+    document.querySelectorAll('form > label').forEach(function (label) {
+        label.innerHTML = wordsKVK[label.id];
+    })
+    document.querySelector('.scene2').style.animationName = animationOut;
+    document.querySelector('.scene2').addEventListener('animationend', function () {
+        document.querySelector('.scene3').style.display = 'block';
+        document.querySelector('.scene3').style.animationName = animationIn;
+        document.querySelector('.scene2').style.display = 'none'
+
+
+    })
+
+
+
+})
+
+document.querySelector('.hk-button').addEventListener('click', function () {
+    document.querySelectorAll('form > label').forEach(function (label) {
+        label.innerHTML = wordsHK[label.id];
+    })
+    document.querySelector('.scene2').style.animationName = animationOut;
+    document.querySelector('.scene2').addEventListener('animationend', function () {
+        document.querySelector('.scene3').style.display = 'block';
+        document.querySelector('.scene3').style.animationName = animationIn;
+        document.querySelector('.scene2').style.display = 'none'
+
+
+    })
+
+}
+)
+
+document.querySelector('.start-button').addEventListener('click', function () {
+    document.querySelector('.scene1').style.animationName = animationOut;
+    //after animation
+    document.querySelector('.scene1').addEventListener('animationend', function () {
+        document.querySelector('.scene2').style.animationDuration = '0.7s';
+        document.querySelector('.scene2').style.animationName = animationIn;
+        document.querySelector('.scene2').style.display = 'block';
+        document.querySelector('.scene1').style.display = 'none'
+
+    })
+
+}
+)
+
+document.querySelector('.reset-button').addEventListener('click', function () {
+    document.querySelector('.background-plain').style.animationName = 'fade-out';
+    document.querySelector('.scene4').style.animationName = animationOut;
+    document.querySelector('.scene4').addEventListener('animationend', function () {
+    window.location.reload();
+    }
+    )
+
+}
+)
