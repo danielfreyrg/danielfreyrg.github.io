f8999111c101ab4561148fbd5507b0d22ceb575d
Daníel Freyr Gylfason <daniel@pipar-tbwa.is>
2024-09-25 18:41:17 +0000

    gsap

--- scroll/scroll.js
+++ scroll/scroll.js
@@ -7,7 +7,6 @@ var test = 0;
 
 const currentFrame = index => (
     `images/${index.toString()}.png`
-    // `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
   )
   
 
