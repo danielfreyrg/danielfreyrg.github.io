e22b1c48ced4992bcbfe64f49ff14904f996dffe
Daníel Freyr Gylfason <daniel@pipar-tbwa.is>
2024-12-11 14:34:50 +0000

    Update script.js

--- tudmundur/script.js
+++ tudmundur/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
-const answers = ['Hættu þessu væli', 'Það er öllum drullu sama', "Skíttu ekki í þig", "þarf ég að hringja í vælubílinn?", "æjæj, elsku rúsínan mín þú hefur það svo erfitt", "er eki kominn tími til að líta aðeins í eigin barm einu sinni?"]
+const answers = ['Hættu þessu væli', 'Það er öllum drullu sama', "Skíttu ekki í þig", "þarf ég að hringja í vælubílinn?", "æjæj, elsku rúsínan mín þú hefur það svo erfitt", "er ekki kominn tími til að líta aðeins í eigin barm einu sinni?"]
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
