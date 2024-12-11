60f58dfc6db17ba2656d2e585c85f4e3649aea72
Daníel Freyr Gylfason <daniel@pipar-tbwa.is>
2024-12-11 15:17:19 +0000

    Update script.js

--- tudmundur/script.js
+++ tudmundur/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
-const answers = ['Hættu þessu væli', 'Það er öllum drullu sama', "Skíttu ekki í þig", "þarf ég að hringja í vælubílinn?", "æjæj, elsku rúsínan mín þú hefur það svo erfitt", "er ekki kominn tími til að líta aðeins í eigin barm einu sinni?", "Á ég að prenta þetta á blað fyrir þig ?", "Þetta var nú ekki fallega sagt, Anna Sigríður mun fá að heyra þetta", "Farðu úr bænum", "heyrðu þú hefur bara rétt fyrir þér", "Djöfulsins læti eru þetta, þarftu ekkert að vinna eða?", "Nei nú hringi ég í jens."]
+const answers = ['Hættu þessu væli', 'Það er öllum drullu sama', "Skíttu ekki í þig", "þarf ég að hringja í vælubílinn?", "æjæj, elsku rúsínan mín þú hefur það svo erfitt", "er ekki kominn tími til að líta aðeins í eigin barm einu sinni?", "Á ég að prenta þetta á blað fyrir þig ?", "Þetta var nú ekki fallega sagt, Anna Sigríður mun fá að heyra þetta", "Farðu úr bænum", "heyrðu þú hefur bara rétt fyrir þér", "Djöfulsins læti eru þetta, þarftu ekkert að vinna eða?", "Nei nú hringi ég í jens.", "Væl og tuðningsmálaráðherra var að  hringja og hún kannaðist ekkert við þig"]
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
